f502808642167e8444032d9c2bbeeeefe5079805
Claire-CXM <15218694777@163.com>
2019-11-15 22:03:24 +0800

    refactor:修正使用element-ui的方式 chore:完成表格组件的修改

--- src/views/menus/accounts/index.vue
+++ src/views/menus/accounts/index.vue
@@ -2,125 +2,154 @@
     <!-- 后台账户管理系统 -->
     <div class="accounts">
         <!-- 添加两个按钮 -->
-        
         <el-button type="primary" value="add" @click="addAccount">添加账号</el-button>
         <el-button type="primary"  value="insert" @click="insertAccount">导入账号</el-button>
-        
-    <main>
-        
         <el-table
             :data="tableData"
             border
             style="width:80%">
             <el-table-column
             fixed
-            prop="account-id"
+            prop="id"
             label="学号"
             align="center"
             width="150">
             </el-table-column>
             <el-table-column
-            prop="account-type"
+            prop="type"
             label="账号类型"
             align="center"
             width="150">
             </el-table-column>
             <el-table-column
-            prop="account-name"
+            prop="name"
             label="姓名"
             align="center"
-            width="100">
+            width="120">
             </el-table-column>
             <el-table-column
-            prop="account-sex"
+            prop="sex"
             label="性别"
             align="center"
             width="100">
             </el-table-column>
             <el-table-column
-            prop="account-phone"
+            prop="phone"
             label="联系方式"
             align="center"
-            width="180">
+            width="200">
             </el-table-column>
             <el-table-column
-            prop="account-deadline"
+            prop="deadline"
             label="账号到期时间"
             align="center"
-            width="180">
+            width="200">
             </el-table-column>
             <el-table-column
             fixed="right"
             align="center"
             label="操作"
             width="120">
-            <template slot-scope="scope">
-                <el-select @click="handleClick(scope.row)" type="text">
-                    <el-option
-                    v-for="item in options"
-                    :key="item.value"
-                    :label="item.label"
-                    :value="item.value">
-                    </el-option>
-                </el-select>
-            </template>
+            <!-- <div class="block">
+            <span class="demonstration"></span>
+            <el-cascader
+                v-model="value"
+                :options="options"
+                @change="handleChange"></el-cascader>
+            </div> -->
+                <el-dropdown height="100%">
+                <span class="el-dropdown-link">
+                    ···<i class="el-icon--right"></i>
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>重置密码</el-dropdown-item>
+                    <el-dropdown-item>修改信息</el-dropdown-item>
+                    <el-dropdown-item>降级为学生账号</el-dropdown-item>
+                    <el-dropdown-item>删除账号</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+            <!-- <template slot-scope="scope">
+                <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+            </template> -->
             </el-table-column>
         </el-table>
-        <!-- <div class="account-list" v-for="(accountItem,index) in accountList" :key="index">
-            <p class="account-type">{{accountItem.type}}</p>
-            <p class="account-name">{{accountItem.name}}</p>
-            <p class="account-sex">{{accountItem.sex}}</p>
-            <p class="account-phone">{{accountItem.phone}}</p>
-            <p class="account-deadline">{{accountItem.deadline}}</p>
-            <p class="account-operate">{{accountItem.operate}}</p>
-        </div>  -->
+            
+        <!-- 添加分页功能 -->
         <el-pagination
             background
             layout="prev, pager, next"
             @current-change="getAccountList"
             :page-count="pageCount">
         </el-pagination>
-
-        </main>
     </div>
 </template>
 
 <script>
 // import responseHandler from '@/utils/responseHandler'
-// import { Button } from 'element-ui'
+import { Table, Button } from 'element-ui'
 export default {
     name: 'Accounts',
     data(){
         return{
+            // value: [],
+            // options: [{
+            //     value: 'ziyuan',
+            //     label: '资源',
+            //     children: [{
+            //         value: 'axure',
+            //         label: 'Axure Components'
+            //     }, {
+            //         value: 'sketch',
+            //         label: 'Sketch Templates'
+            //     }, {
+            //         value: 'jiaohu',
+            //         label: '组件交互文档'
+            //     }]
+            // }],
             tableData: [{
-                accountId: '171100247',
-                accountType: '学生管理员',
-                accoutnName: '蔡小小',
-                accountSex: '女',
-                accountPhone: '742777',
-                accountDeadline: '2019.11.12',
-                pageCount: 1,
+                id: '171100247',
+                type: '学生管理员',
+                name: '蔡小小',
+                sex: '女',
+                phone: '742777',
+                deadline: '2019.11.12',
+                // pageCount: 1,
+                accountList: [
+                
+                ] }, {
+                id: '171100247',
+                type: '学生管理员',
+                name: '蔡小小',
+                sex: '女',
+                phone: '742777',
+                deadline: '2019.11.12',
+                // pageCount: 1,
+                accountList: [
+                
+                ] }, {
+                id: '171100247',
+                type: '学生管理员',
+                name: '蔡小小',
+                sex: '女',
+                phone: '742777',
+                deadline: '2019.11.12',
+                // pageCount: 1,
+                accountList: [
+                
+                ] }, {
+                id: '171100247',
+                type: '学生管理员',
+                name: '蔡小小',
+                sex: '女',
+                phone: '742777',
+                deadline: '2019.11.12',
+                // pageCount: 1,
                 accountList: [
                 
                 ] }
-            ],
-            options: [{
-                value: '1',
-                label: '重置密码'
-            }, {
-                value: '2',
-                label: '修改信息'
-            }, {
-                value: '3',
-                label: '降级为学生账号'
-            }, {
-                value: '4',
-                label: '删除账号'
-            } ],
-            value: ''
+            ]
         }
     },
-    
     methods: {
         addAccount(){
             
@@ -147,12 +176,20 @@ export default {
     // display: flex;
     // flex-direction: column;
     .el-button{
-        margin: 30px 20px 30px 36px;
+        margin: 30px 0 30px 36px;
     }
     .el-table{
         margin-left: 36px;
         margin-bottom: 30px;
-
+    }
+    .el-dropdown-link {
+        color: #409EFF;
+        &:hover {
+            cursor: pointer;
+        }
+    }
+    .el-icon-arrow-down {
+        font-size: 12px
     }
     .el-pagination{
         margin:0 auto;
