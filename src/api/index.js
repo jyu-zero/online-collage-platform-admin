90c497cbe259bd40e5f46210624d21879b366789
kerry <1457948957@qq.com>
2019-11-19 20:58:40 +0800

    chore:修改了总index.js存在的问题

--- src/api/index.js
+++ src/api/index.js
@@ -5,15 +5,12 @@ import news from './news'
 import apiPrefix from './apiPrefix'
 import _responseHandler from './responseHandler'
 import question from './question'
-import dutyScheduling from './duty-scheduling'
 
 export const userApi = user
 
 export const dutySchedulingApi = dutyScheduling
 
 export const newsApi = news
-
-export const dutySchedulingApi = dutyScheduling
 
 export const testApi = { test: '/test' }
 
