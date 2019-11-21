0ac612d8a2682224f4065abb7d899f2dcef7e33e
lilllj <413775061@qq.com>
2019-11-21 16:38:29 +0800

    feat:完成添加账号的部分功能

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -3,5 +3,7 @@ export default {
     // 显示今日新增的失物
     'todayAdd': '/admin/goods/today-add',
     // 获取今天的值班安排
-    'getTodayArrange': '/admin/get-today-arrange'
+    'getTodayArrange': '/admin/get-today-arrange',
+    // 获取添加账号中的信息
+    'addAccount': '/admin/students/create-students'
 }
--- src/views/menus/accounts/index.vue
+++ src/views/menus/accounts/index.vue
@@ -79,29 +79,38 @@
           layout="prev, pager, next"
           :total="50">
         </el-pagination>
-        <!-- 出现添加账号操作框 -->
+        <!-- 出现添加账号对话框 -->
         <el-dialog
         title="添加账号"
         :visible.sync="dialogVisible"
         width="30%"
         :before-close="handleClose">
-        <p>请输入账号:</p>
+        <p>请输入学号:</p>
+        <el-input v-model="account" placeholder="请输入学号" ></el-input>
         <p>请输入姓名:</p>
+        <el-input v-model="name" placeholder="请输入姓名"></el-input>
         <p>请输入性别:</p>
+        <el-input v-model="sex" placeholder="请输入性别"></el-input>
         <p>请输入联系方式:</p>
+        <el-input v-model="contact" placeholder="请输入联系方式"></el-input>
         <p>请输入班级年级:</p>
+        <el-input v-model="class_grade" placeholder="请输入班级年级"></el-input>
         <p>请输入宿舍:</p>
+        <el-input v-model="dormitory" placeholder="请输入宿舍"></el-input>
         <span slot="footer" class="dialog-footer">
             <el-button @click="dialogVisible = false">取 消</el-button>
             <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
         </span>
         </el-dialog>
-        <!-- 添加账号操作框【完】 -->
+        <!-- 添加账号对话框【完】 -->
+        <!-- 导入账号对话框 -->
+
+        <!-- 导入账号对话框【完】 -->
     </div>
 </template>
 
 <script>
-// import responseHandler from '@/utils/responseHandler'
+import { userApi, prefix, responseHandler } from '@/api'
 import { Button, Table, Input, Message, Pagination, Dropdown, Select, Dialog } from 'element-ui'
 export default {
     name: 'Accounts',
@@ -117,7 +126,17 @@ export default {
     },
     data() {
         return {
+            // 是否弹出添加账号对话框
             dialogVisible: false,
+            // 添加账号中的内容
+            account: '',
+            name: '',
+            sex: '',
+            contact: '',
+            class_grade: '',
+            dormitory: '',
+            // 添加账号中的内容【完】
+            // 表格中的内容
             tableData: [{
                 num: '151110058',
                 name: '王小虎',
@@ -161,16 +180,41 @@ export default {
                 grade: '1503',
                 dorm: '南7-201'
             }]
+            // 表格中的内容【完】
 
         }
     },
     methods: {
+        // 添加账号框却认关闭窗口
         handleClose(done) {
             this.$confirm('确认关闭？')
                 .then(_ => {
                     done()
                 })
                 .catch(_ => {})
+        },
+        // 获取添加账号中的内容
+        addAccount(){
+            // 判断输入框是否为空
+            if(this.account === '' || this.name === '' || this.sex === '' || this.contact === '' || this.class_grade === '' || this.dormitory === ''){
+                Message.error('输入框不能为空！')
+                return
+            }
+            this.$axios.post(prefix.api + userApi.addAccount, {
+                account: this.account,
+                name: this.name,
+                sex: this.sex,
+                contact: this.contact,
+                class_grade: this.class_grade,
+                dormitory: this.dormitory
+            }).then((response)=>{
+                console.log(response)
+                if(!responseHandler(response.data, this)){
+                    Message.error('请求失败')
+                }
+                Message.success('请求成功')
+            })
+            this.dialogVisible(false)
         }
     }
 }
