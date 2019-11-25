c5fb04c09f2ec605b91634833d7e8ddd774d6729
GWolfQAQQ <1536585769@qq.com>
2019-11-25 22:48:50 +0800

    chore:完成GWolf代码合并到index.vue

--- src/views/menus/duty-scheduling/index.vue
+++ src/views/menus/duty-scheduling/index.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="duty">
         <div id="btn-set" class="button-group">
+            <div>
             <!-- TODO:整合金隆安排表 -->
-            <el-button type="primary" @click="beginToArrange">安排人员</el-button>
-            <!-- 清空安排表数据库 -->
-            <el-button type="primary" @click="resetArrange">重置所有安排</el-button>
-            <!-- 自加：清空无课表 -->
-            <el-button type="primary" @click="resetFreelnformation">重置无课表</el-button>
+                <el-button type="primary" @click="beginToArrange">安排人员</el-button>
+                <!-- 清空安排表数据库 -->
+                <el-button type="primary" @click="resetArrange">重置所有安排</el-button>
+                <!-- 自加：清空无课表 -->
+                <el-button type="primary" @click="resetFreelnformation">重置无课表</el-button>
+               
+            </div>
             <!-- TODO:整合棉伟登记无课表 -->
+            <el-button type='primary' @click='toggle'>登记无课表</el-button>
             <!-- <el-button type="primary" @click="goToRegister">登记无课表</el-button> -->
         </div>
 
@@ -90,12 +94,43 @@
                 </div>
             </div>
         </div>
+         <!-- 这里开始是GWolf写的 -->
+        <div class='course-container' v-if='visibility'>
+            <h3>登记无课表</h3>
+      
+            <div class='record-course'>
+                <table border='1' cellspacing='0' cellpadding='0'>
+                    <tr>
+                        <th class='day' v-for='(item, index) in weekday' :key='index'>
+                            {{ item }}
+                        </th>
+                    </tr>
+                    <tr v-for='(itemss, index) in idArr' :key='index'>
+                        <td
+                            v-for='(items, index) in itemss'
+                            :key='index'
+                            @click='saveId(items)'
+                        >
+                            <course @add-to-arr='addId'></course>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class='btn-container'>
+                <el-button class='button' @click='cancelFreeCourse'>取消</el-button>
+                <el-button type='primary' class='button' @click='submit'
+                >确定</el-button
+                >
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import { Button, Select, Option, Switch, Table, TableColumn, Message } from 'element-ui'
 import { prefix, dutySchedulingApi, responseHandler } from '@/api'
+import course from '@/components/duty-scheduling/Course.vue'
 
 export default {
     name: 'DutyScheduling',
@@ -106,7 +141,8 @@ export default {
         [Switch.name]: Switch,
         [Table.name]: Table,
         [TableColumn.name]: TableColumn,
-        [Message.name]: Message
+        [Message.name]: Message,
+        course
     },
     props: {
         row: Number,
@@ -309,6 +345,65 @@ export default {
         },
         cancelArrange() {
             this.isActive = false
+        },
+        cancelFreeCourse() {
+            this.visibility = !this.visibility
+        },
+        // 无课表显示
+        toggle() {
+            this.visibility = !this.visibility
+        },
+        // 保存课程对应的id
+        saveId(items) {
+            this.id = Number(items)
+            console.log(this.id)
+        },
+        // 将id保存到数组中
+        addId(data) {
+            switch (data) {
+                case '单周无课':
+                    if (this.saveIdArr.indexOf(this.id) >= 0) {
+                        return
+                    }
+                    this.saveIdArr.push(this.id)
+                    break
+
+                case '双周无课':
+                    this.id = Number(this.id + 20)
+                    if (this.saveIdArr.indexOf(this.id) >= 0) {
+                        return
+                    }
+                    this.saveIdArr.push(this.id)
+                    break
+
+                case '单双无课':
+                    if (this.saveIdArr.indexOf(this.id) >= 0) {
+                        return
+                    }
+                    this.saveIdArr.push(this.id)
+                    this.id = Number(this.id + 20)
+                    if (this.saveIdArr.indexOf(this.id) >= 0) {
+                        return
+                    }
+                    this.saveIdArr.push(this.id)
+                    break
+                case '单双有课':
+                    break
+            }
+            console.log(this.saveIdArr)
+        },
+        // 提交id数组
+        submit() {
+            this.axios
+                .post(prefix.api + dutySchedulingApi.submitFreeInformation, {
+                    freeCourseList: this.saveIdArr
+                })
+                .then(response => {
+                    if (!responseHandler.handle(response.data, this)) {
+                        return
+                    }
+                    console.log('success')
+                })
         }
     },
     data() {
@@ -447,13 +542,29 @@ export default {
             options: [],
             arrange: [],
             week: false,
-            isActive: false
+            isActive: false,
+
+            visibility: false,
+            weekday: ['星期一', '星期二', '星期三', '星期四', '星期五'],
+            idArr: [
+                ['1', '2', '3', '4', '5'],
+                ['6', '7', '8', '9', '10'],
+                ['11', '12', '13', '14', '15'],
+                ['16', '17', '18', '19', '20'],
+                ['21', '22', '23', '24', '25'],
+                ['26', '27', '28', '29', '30'],
+                ['31', '32', '33', '34', '35'],
+                ['36', '37', '38', '39', '40']
+            ],
+            saveIdArr: [],
+            id: 0
         }
     }
 }
 </script>
 
 <style lang="less" scoped>
+@width: 100px;
 .duty {
     position: relative;
     height: 100%;
@@ -465,6 +576,9 @@ export default {
     margin-top: 2%;
     margin-bottom: 2%;
     padding-left: 2%;
+    padding-right: 4%;
+    display: flex;
+    justify-content: space-between;
     & button{
         width:120px;
         display: inline-block;
@@ -566,4 +680,42 @@ a {
 // .arrange-bts-group{
 //     align-self: flex-end;
 // }
+
+h3 {
+  margin: 0;
+  margin-bottom: 10px;
+}
+.course-container {
+  box-sizing: border-box;
+  width: @width*5+47px;
+  padding: 20px;
+  position: fixed;
+  top:25%;
+  left:40%;
+  border: 1px solid black;
+}
+.record-course {
+  width: @width*5+7px;
+  box-sizing: border-box;
+}
+
+table {
+  border-collapse: collapse;
+  table-layout: fixed;
+}
+td {
+  width: @width;
+  height: 32px;
+}
+.day {
+  line-height: 40px;
+  text-align: center;
+}
+.btn-container {
+  display: flex;
+  justify-content: flex-end;
+}
+.button {
+  margin-top: 15px;
+}
 </style>
